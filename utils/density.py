10e31f4eba4db34b8bcdcd78c8753d45f4b6f36b
ejVasco <111936685+ejVasco@users.noreply.github.com>
2026-02-05 10:19:14 -0800

    density function and a test script

--- utils/density.py
+++ utils/density.py
@@ -1,1 +1,32 @@
 # script for generating density maps
+
+import numpy as np
+from scipy.ndimage import gaussian_filter
+
+
+def gen_density_map(image_shape, points, sigma =4):
+    """
+    Args
+        auau
+    Returns
+        density_map:
+    """
+
+    H, W = image_shape
+    density = np.zeros((H,W), dtype=np.float32)
+
+    if len(points) ==0:
+        return density # exit if no labeled points in json
+    
+    for x, y in points:
+        x = int(round(x))
+        y = int(round(y))
+        
+        if x<0 or x>=W or y<0 or y>=H:
+            continue # skips if somehow there are points outside the image
+        
+        density[y,x] += 1.0 # set density at point locations
+    
+    density = gaussian_filter(density, sigma=sigma)
+    
+    return density
---
+++ utils/test_density.py
@@ -0,0 +1,42 @@
+# script to test density.py
+
+import json 
+import os
+import numpy as np 
+import matplotlib.pyplot as plt
+from PIL import Image 
+
+from density import gen_density_map
+
+# paths
+IMG_DIR = "../frames/GX011278-fish-1-10/images"
+ANN_PATH = "../frames/GX011278-fish-1-10/annotations.json"
+
+# selected image 
+IMG_NAME="image_0.jpg"
+
+img_path = os.path.join(IMG_DIR, IMG_NAME)
+img = Image.open(img_path).convert("RGB")
+img_np = np.array(img)
+
+H,W, _ = img_np.shape 
+
+with open(ANN_PATH, "r") as f:
+    annotations = json.load(f) 
+    
+points = annotations[IMG_NAME]
+
+density = gen_density_map((H,W), points, sigma=4)
+
+print(f"number of points: {len(points)}")
+print(f"density sum: {density.sum():.4f}")
+
+plt.figure(figsize=(12,5))
+
+plt.subplot(1,2,1)
+plt.title("og img")
+plt.imshow(density, cmap="jet")
+plt.colorbar()
+plt.axis("off")
+
+plt.show()
