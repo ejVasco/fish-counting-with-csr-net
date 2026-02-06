0dfb484a81deaaea6e4e1eea7b814c851bd1e3dc
ejVasco <111936685+ejVasco@users.noreply.github.com>
2026-02-05 16:49:58 -0800

    simple comments

--- datasets/test_dataset.py
+++ datasets/test_dataset.py
@@ -1,13 +1,18 @@
 from datasets.fish_dataset import FishDataset
 
+# list of folders to turn into datasets
 TRAIN_DATASETS = [
     "GX011278-fish-1-10",
 ]
 
+# initialize FishDataset class
 dataset = FishDataset("datasets", TRAIN_DATASETS)
 
+# fetch first dataset item
 img, den = dataset[0]
 
+# prints image tensor shape
 print(img.shape)
+# prints density map tensor shape and sum
 print(den.shape)
 print(den.sum())
