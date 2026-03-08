eb4008b743e4cc6f8c22db4c5c6ac6f0b5b21aca
ejVasco <111936685+ejVasco@users.noreply.github.com>
2026-03-07 20:35:31 -0800

    implemented resizing images that are too high resolution

--- datasets/fish_dataset.py
+++ datasets/fish_dataset.py
@@ -8,13 +8,15 @@ from torch.utils.data import Dataset
 from utils.density import gen_density_map  
 
 class FishDataset(Dataset): #custom dataset inherited from base "Dataset" from torch.utils
-    def __init__(self, root_dir, dataset_names):
+    def __init__(self, root_dir, dataset_names, max_size=1024):
+        # max size is used for resizing images
         """
         Args:
             root_dir: root folder
             dataset_names: list of dataset folder identifiers
         """
         self.samples = [] # list for storing dictionaries of image paths and points
+        self.max_size = max_size
 
         for dataset_name in dataset_names:
             # create paths
@@ -47,26 +49,40 @@ class FishDataset(Dataset): #custom dataset inherited from base "Dataset" from t
         # loading image
         image = cv2.imread(sample["image_path"])
         if image is None:
-            raise RuntimeError(f"failed to load image: {sample["image_path"]}") 
+            raise RuntimeError(f"failed to load image: {sample['image_path']}")
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = image.astype(np.float32)/255.0 # converts from ints to floats (0-255)->(0.0-1.0)
 
         H, W, _ = image.shape
+        
+        # resize images 
+        scale = 1.0
+        if self.max_size is not None: 
+            scale = min(self.max_size / W, self.max_size / H, 1.0)
+        new_W = int(W* scale)
+        new_H = int(H * scale)
+        if scale != 1.0:
+            image = cv2.resize(image, (new_W, new_H))
+
+        # resize points
+        points = sample["points"]
+        scaled_points = [] 
+        for x,y in points:
+            scaled_points.append((x*scale, y*scale))
 
         # generate density map
-        points = sample["points"]
-        density = gen_density_map((H, W), points)
+        density = gen_density_map((new_H, new_W), scaled_points)
 
         # csrnet downsamples by 8 
         density = cv2.resize(
             density,
-            (W // 8, H // 8),
+            (new_W // 8, new_H // 8),
             interpolation=cv2.INTER_CUBIC
         )
 
         # adjust so density sum reains the same after downsamples
-        density *= (H*W) / ((H // 8) * (W // 8))
+        density *= (new_H*new_W) / ((new_H // 8) * (new_W // 8))
 
         image = torch.from_numpy(image).permute(2,0,1) # convert to pytorch format (H,W,C)->(C,H,W)
         density = torch.from_numpy(density).unsqueeze(0) # add a channel dimension (reuired by cnn)
